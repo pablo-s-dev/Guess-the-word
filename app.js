998276fda708707c8f754bba8e57bdeb19f9ac7a
ps <ps.login.username@gmail.com>
2022-11-16 21:00:08 -0200

    words and defs need some improvement yet, first version

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port);
